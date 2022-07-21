4b6262bc0bb74c034bd1dc33eaae21c31fe7ec67
Jessica Faustino <jessicacfaustino@gmail.com>
2022-07-20 22:20:12 -0300

    Conclusao do exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const num1 = prompt ("Digite um numero par: ");
+console.log ('Numero: ', num1);
+
+const resto = Number(num1 % 2) ; //Sempre que um número for par o resto da divisão é 0, quando é ímpar o resto da divisão é 1
+console.log ('Resto da divisão', resto); 
