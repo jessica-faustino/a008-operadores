4f62998f668be2cff02cf02c7e223b3a9dc90f63
Jessica Faustino <jessicacfaustino@gmail.com>
2022-10-25 11:07:39 -0300

    Subindo o repositório

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,7 +1,7 @@
-const a = prompt ('Digite o primeiro número: ');
+ /*const a = prompt ('Digite o primeiro número: ');
 console.log ('A = ',a);
 
-const b = prompt ('Digite o segundo número: ');
+ const b = prompt ('Digite o segundo número: ');
 console.log ('B = ', b);
 
 resultado1 = a > b;
@@ -13,5 +13,21 @@ console.log ('O primeiro numero é igual ao segundo?', resultado2);
 resultado3 = a%b == 0;
 console.log ('O primeiro numero é divisível pelo segundo?', resultado3);
 
-resultado4 = b%a ==0;
-console.log ('O segundo numero é divisível pelo primeiro?', resultado4);
+resultado4 = b%a == 0;
+console.log ('O segundo numero é divisível pelo primeiro?', resultado4); */
+
+let a = prompt ("Digite o primeiro numero: ");
+console.log ("A: ", a);
+
+while (a != 0){
+
+for (let cont = 1 ; cont <= 9 ; cont ++){
+    
+    a = a * cont;
+    console.log (a);
+
+    let a = prompt ('Digite um número: ');
+    console.log ('A = ',a);   
+}
+
+}
